4134ce51efbd7f621939c9c3e16437ab0bf5c970
Esraq <esraq26552@gmail.com>
2023-01-27 11:30:51 +0600

    Addition.Substraction,Multipication,Dvision

---
+++ programme_11.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+	
+	int a,b,addition,substraction,multipication,division;
+	
+	
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	
+	addition=a+b;
+	substraction=a-b;
+	multipication=a*b;
+	division=a/b;
+	
+	
+	
+	printf("Addition=%d\n",addition);
+	printf("Substraction=%d\n",substraction);
+	printf("Multipication=%d\n",multipication);
+	printf("Division=%d\n",division);
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
