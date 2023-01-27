f041490bdfdacd62a1b039c889339236a1f2031d
Esraq <esraq26552@gmail.com>
2023-01-27 11:14:37 +0600

    Integer addition

---
+++ programme_6.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	
+	int a,b,add;
+	
+	
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	
+	add=a+b;
+	
+	printf("Sum=%d",add);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
