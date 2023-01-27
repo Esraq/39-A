bc70cb99c2385a66bbb22a8ccccf5e83fc5b3d93
Esraq <esraq26552@gmail.com>
2023-01-27 11:25:12 +0600

    Divisiblity

---
+++ programme_10.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	
+	int a,b,divisible;
+	
+	
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	
+	divisible=a%b;
+	
+	printf("Divisible=%d",divisible);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
