fccf6014193e36726f21aa9c4c6fc153c6cd2ec0
Esraq <esraq26552@gmail.com>
2023-01-27 11:22:51 +0600

    Division

---
+++ programme_9.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	
+	int a,b,div;
+	
+	
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	
+	div=a/b;
+	
+	printf("Division=%d",div);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
