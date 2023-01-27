077db2f4e8a942a011a040cf24e856aaa937c316
Esraq <esraq26552@gmail.com>
2023-01-27 11:18:21 +0600

    Substraction

---
+++ programme_7.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	
+	int a,b,sub;
+	
+	
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	
+	sub=a-b;
+	
+	printf("Substraction=%d",sub);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
