ad42ccfe02d3ea5f7063649e93edc529a214270b
Esraq <esraq26552@gmail.com>
2023-01-27 10:48:31 +0600

    New Line

---
+++ programme_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	
+	
+	
+	printf("Esraq Humayun\n"); /// /n jokhon disi newline create hoisa
+	printf("I am from Daffodil International University");
+	
+	
+	
+	
+	
+	return 0;
+}
