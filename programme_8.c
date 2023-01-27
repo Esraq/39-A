99e48fb82097ec18dbdb825121df4269eeee845d
Esraq <esraq26552@gmail.com>
2023-01-27 11:20:13 +0600

    Multipication-Code

---
+++ programme_8.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	
+	int a,b,mul;
+	
+	
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	
+	mul=a*b;
+	
+	printf("Multipication=%d",mul);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
